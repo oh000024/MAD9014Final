f154924a33593ebef0b98d8f05d15ab32a23f231
oh000024 <oh000024@algonquinlive.com>
2016-12-13 00:08:02 -0500

    [Del] Delete comments

--- www/js/index.js
+++ www/js/index.js
@@ -1,167 +1,110 @@
 "use strict"
-
 if (document.deviceready) {
     document.addEventlistener('deviceready', onDeviceReady);
-} else {
+}
+else {
     document.addEventListener('DOMContentLoaded', onDeviceReady)
 }
-
 const GLOBALKEY = "oh000024scores";
-
 var gTeamInfo = []; //new Map();
 var gdata = [];
 
 function onDeviceReady() {
-	console.log("Ready!");
-
-	pages = document.querySelectorAll('[data-role="page"]');
-	links = document.querySelectorAll('[data-role="nav"] a');
-
-	for (let i = 0; i < links.length; i++) {
-		links[i].addEventListener("click", navigate);
-	}
-	document.querySelector(".refresh").addEventListener("click", onRefreshData);
-
-	if (localStorage.getItem(GLOBALKEY)) {
-
-		gdata = JSON.parse(localStorage.getItem(GLOBALKEY));
-		displayData(gdata, false);
-
-	} else {
-		serverData.getJSON();
-	}
-	
-	let thwin = document.querySelector(".wins");
-	thwin.addEventListener("click",onClickWins);
-	
-	let thlosses = document.querySelector(".losses");
-	thlosses.addEventListener("click",onClickLosses);
-	
-	let ththies = document.querySelector(".ties");
-	ththies.addEventListener("click",onClickTies);
-	
-	let thpoints = document.querySelector(".points");
-	thpoints.addEventListener("click",onClickPoints);	
+    console.log("Ready!");
+    pages = document.querySelectorAll('[data-role="page"]');
+    links = document.querySelectorAll('[data-role="nav"] a');
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", navigate);
+    }
+    document.querySelector(".refresh").addEventListener("click", onRefreshData);
+    if (localStorage.getItem(GLOBALKEY)) {
+        gdata = JSON.parse(localStorage.getItem(GLOBALKEY));
+        displayData(gdata, false);
+    }
+    else {
+        serverData.getJSON();
+    }
+    let thwin = document.querySelector(".wins");
+    thwin.addEventListener("click", onClickWins);
+    let thlosses = document.querySelector(".losses");
+    thlosses.addEventListener("click", onClickLosses);
+    let ththies = document.querySelector(".ties");
+    ththies.addEventListener("click", onClickTies);
+    let thpoints = document.querySelector(".points");
+    thpoints.addEventListener("click", onClickPoints);
 }
-
 let serverData = {
-    	url: "http://griffis.edumedia.ca/mad9014/sports/quidditch.php",
-    httpRequest: "GET",
-    getJSON: function () {
-
+    url: "http://griffis.edumedia.ca/mad9014/sports/quidditch.php"
+    , httpRequest: "GET"
+    , getJSON: function () {
         let headers = new Headers();
-
         headers.append("Content-Type", "text/plain");
         headers.append("Accept", "application/json; charset=utf-8");
-        
         // simply show them in the console
         console.dir("headers: " + headers.get("Content-Type"));
         console.dir("headers: " + headers.get("Accept"));
-        
         // Now the best way to get this data all together is to use an options object:
-        
         // Create an options object
         let options = {
-            method: serverData.httpRequest,
-            mode: "cors",
-            headers: headers
+            method: serverData.httpRequest
+            , mode: "cors"
+            , headers: headers
         };
-        
         // Create an request object so everything we need is in one package
         let request = new Request(serverData.url, options);
         console.log(request);
-           
-        fetch(request)
-            .then(function (response) {
-
-                console.log(response);
-                return response.json();
-            })
-            .then(function (data) {
-                // console.log(data); // now we have JS data, let's display it
-            
-                //localStorage.setItem(GLOBALKEY, JSON.stringify(data));
-
-                // Call a function that uses the data we recieved  
-                displayData(data,true);
-            })
-            .catch(function (err) {
-                alert("Error: " + err.message);
-            });
+        fetch(request).then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (data) {
+            // console.log(data); // now we have JS data, let's display it
+            //localStorage.setItem(GLOBALKEY, JSON.stringify(data));
+            // Call a function that uses the data we recieved  
+            displayData(data, true);
+        }).catch(function (err) {
+            alert("Error: " + err.message);
+        });
     }
 };
+
 function displayData(data, bUpdate) {
-    
-	gTeamInfo.length = 0;
-	if (true == bUpdate) {
-		localStorage.setItem(GLOBALKEY, JSON.stringify(data));
-	}
-	data.teams.forEach(function (team) {
-		var teaminfo = {
-				teamname: team.name,
-				wins: 0,
-				losses: 0,
-				ties: 0,
-				points: 0
-			}
-			
-		gTeamInfo[team.id] = teaminfo;
-	});
-	
-
-//		value.games.forEach(function (item) {
-
-//			homeTeam = getTeamName(data.teams, item.home);
-//			awayTeam = getTeamName(data.teams, item.away);
-//
-//			let dg = document.createElement("div");
-//
-//			let home = document.createElement("div");
-//			home.innerHTML = homeTeam + " " + "<b>" + item.home_score + "</b>" + "&nbsp" + "<br>";
-//			//home.style.backgroundColor = "#C4EDFF";
-//			//            
-//			let away = document.createElement("div");
-//			away.innerHTML = awayTeam + " " + "<b>" + item.away_score + "</b>" + "&nbsp" + "<br>";
-//			//away.style.backgroundColor = "#C4EDDE";
-//			//            
-//			//dg.style.backgroundcolor="red";
-//			dg.appendChild(home);
-//			dg.appendChild(away);
-//			dg.innerHTML += "<hr>";
-//			ul.appendChild(dg);
-			
-
-//		});
-       
-        displayGameData(data.scores);
-		//StandingsData(value);
-
-//	});
-	gTeamInfo.sort(function (a, b) {
-		if (a.wins < b.wins) {
-			return 1;
-		}
-		if (a.wins > b.wins) {
-			return -1;
-		}
-		// a must be equal to b
-		return 0;
-	});
-	displayStandingData();
-
+    gTeamInfo.length = 0;
+    if (true == bUpdate) {
+        localStorage.setItem(GLOBALKEY, JSON.stringify(data));
+    }
+    data.teams.forEach(function (team) {
+        var teaminfo = {
+            teamname: team.name
+            , wins: 0
+            , losses: 0
+            , ties: 0
+            , points: 0
+        }
+        gTeamInfo[team.id] = teaminfo;
+    });
+    displayGameData(data.scores);
+    gTeamInfo.sort(function (a, b) {
+        if (a.wins < b.wins) {
+            return 1;
+        }
+        if (a.wins > b.wins) {
+            return -1;
+        }
+        // a must be equal to b
+        return 0;
+    });
+    displayStandingData();
 }
 
 function onRefreshData() {
-	serverData.getJSON();
+    serverData.getJSON();
 }
 
 function sortMapByValue(mapdata) {
-	var arr = [];
-	for (var key of mapdata)
-		arr.push(key[1]);
-
-	arr.sort(function (a, b) {
-		return a.wins - b.wins;
-	});
-	return arr; // returns array
+    var arr = [];
+    for (var key of mapdata) arr.push(key[1]);
+    arr.sort(function (a, b) {
+        return a.wins - b.wins;
+    });
+    return arr; // returns array
 }
