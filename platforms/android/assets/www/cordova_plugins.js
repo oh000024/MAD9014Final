a3088bcc6f93c618271f245a817f99dd80bd4df8
oh000024 <oh000024@algonquinlive.com>
2016-12-13 17:22:22 -0500

    [Add]Plugin Console

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -22,7 +22,8 @@ module.exports.metadata =
 {
     "cordova-plugin-device": "1.1.4",
     "cordova-plugin-whitelist": "1.3.1",
-    "cordova-plugin-splashscreen": "4.0.1"
+    "cordova-plugin-splashscreen": "4.0.1",
+    "cordova-plugin-console": "1.0.5"
 };
 // BOTTOM OF METADATA
 });
